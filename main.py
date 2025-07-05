b91b949c28492468e0ed12aaf2457e6e7aca692a
Araz saber <arazsaber44@gmail.com>
2025-07-05 18:51:16 +0330

    1
    3

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "8052658426:AAFDSXMIhzLH40RY1zGC0DVHHytaj5W6_Zs"  # جای YOUR_BOT_TOKEN را با توکن ربات خود جایگزین کنید
+TOKEN = "8052658426:AAFDSXMIhzLH40RY1zGC0DVHHytaj5W6_Zs"
 
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('سلام! به ربات من خوش آمدید!')
-
-def echo(update: Update, context: CallbackContext) -> None:
-    user_message = update.message.text
-    update.message.reply_text(f'شما گفتید: {user_message}')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! من یه ربات بیت‌کوین هستم")
 
 def main():
-    updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(filters.text & ~filters.command, echo))
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
